8bde96f0ac10cce2f11e6f47084c34ae155dbe86
yoglockie <yogilokesh916@gmail.com>
2023-08-25 17:17:47 +0530

    hooks

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Hook from './Component/Hook';
+
+function App() {
+  return (
+   <Hook/>
+  );
+}
+
+export default App;
---
+++ src/Component/Hook.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Button from "@mui/material/Button";
+
+export default function Hook() {
+    const [count,setcount]=useState(0);
+    function Incre() {
+        setcount(count+1)
+    }
+    function Decre() {
+        if(count<=0)
+        {
+            alert("Value less than 0")
+            return
+        }
+        else{
+            setcount(count-1)
+        }
+    }
+  return (
+
+       <>
+       <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+          
+          <h1 style={{display:'flex',justifyContent:'center',alignItems:'center',height:"200px",width:"200px", backgroundColor:"#1976D2",color:'white' ,borderRadius:"20px"}}>{count}</h1>
+          
+          
+       </div>
+
+       <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+        <Button variant="contained" onClick={Incre} style={{margin:"10px"}}>+</Button>
+          <Button variant="contained" onClick={Decre}>-</Button>
+       </div>
+       </>
+  ) 
+}
